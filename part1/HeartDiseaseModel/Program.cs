0664659634b1253f80ef797b9549a56c997a0da2
Nat Kershaw <nakersha@microsoft.com>
2019-06-06 21:19:45 -0700

    Add part1 complete

---
+++ part1/HeartDiseaseModel/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using HeartDiseaseModel.DataStructures;
+using Microsoft.ML;
+using static Microsoft.ML.DataOperationsCatalog;
+
+namespace HeartDiseaseModel
+{
+    public class Program
+    {
+        private static string BaseDatasetsRelativePath = @"data";
+        private static string TrainDataPath = $"{BaseDatasetsRelativePath}/training.csv";
+        private static string TestDataPath = $"{BaseDatasetsRelativePath}/test.csv";
+
+        private static string BaseModelsRelativePath = @"models";
+        private static string ModelPath = $"{BaseModelsRelativePath}/HeartClassification.zip";
+        
+        public static void Main(string[] args)
+        {
+            var mlContext = new MLContext();
+            BuildTrainEvaluateAndSaveModel(mlContext);
+
+            TestPrediction(mlContext);
+
+            Console.WriteLine("=============== End of process, hit any key to finish ===============");
+        }
+
+        private static void BuildTrainEvaluateAndSaveModel(MLContext mlContext)
+        {
+            // STEP 1: Load data
+            IDataView trainingDataView = mlContext.Data.LoadFromTextFile<HeartData>(TrainDataPath, hasHeader: true, separatorChar: ';');
+
+            // STEP 2: Create pipeline
+            var pipeline = mlContext.Transforms.Concatenate("Features", "Age", "Sex", "Cp", "TrestBps", "Chol", "Fbs", "RestEcg", "Thalac", "Exang", "OldPeak", "Slope", "Ca", "Thal")
+                .Append(mlContext.BinaryClassification.Trainers.FastTree(labelColumnName: "Label", featureColumnName: "Features"));
+
+            // STEP 3: Train the model
+            Console.WriteLine("=============== Training the model ===============");
+            ITransformer trainedModel = pipeline.Fit(trainingDataView);
+
+            // STEP 4: Evaluate the model against test data
+            IDataView testDataView = mlContext.Data.LoadFromTextFile<HeartData>(TestDataPath, hasHeader: true, separatorChar: ';');
+            Console.WriteLine("===== Evaluating Model's accuracy with Test data =====");
+            var predictions = trainedModel.Transform(testDataView);
+
+            var metrics = mlContext.BinaryClassification.Evaluate(data: predictions, labelColumnName: "Label", scoreColumnName: "Score");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine($"************************************************************");
+            Console.WriteLine($"*       Metrics for {trainedModel.ToString()} binary classification model      ");
+            Console.WriteLine($"*-----------------------------------------------------------");
+            Console.WriteLine($"*       Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"*       Area Under Roc Curve:      {metrics.AreaUnderRocCurve:P2}");
+            Console.WriteLine($"*       Area Under PrecisionRecall Curve:  {metrics.AreaUnderPrecisionRecallCurve:P2}");
+            Console.WriteLine($"*       F1Score:  {metrics.F1Score:P2}");
+            Console.WriteLine($"*       LogLoss:  {metrics.LogLoss:#.##}");
+            Console.WriteLine($"*       LogLossReduction:  {metrics.LogLossReduction:#.##}");
+            Console.WriteLine($"*       PositivePrecision:  {metrics.PositivePrecision:#.##}");
+            Console.WriteLine($"*       PositiveRecall:  {metrics.PositiveRecall:#.##}");
+            Console.WriteLine($"*       NegativePrecision:  {metrics.NegativePrecision:#.##}");
+            Console.WriteLine($"*       NegativeRecall:  {metrics.NegativeRecall:P2}");
+            Console.WriteLine($"************************************************************");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+
+            // STEP 5: Save the model to a zip file, to be consumed by an application
+            Console.WriteLine("=============== Saving the model to a file ===============");
+            mlContext.Model.Save(trainedModel, trainingDataView.Schema, ModelPath);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("=============== Model Saved ============= ");
+        }
+
+
+        private static void TestPrediction(MLContext mlContext)
+        {
+            ITransformer trainedModel = mlContext.Model.Load(ModelPath, out var modelInputSchema);
+
+            // Create prediction engine related to the loaded trained model
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<HeartData, HeartPrediction>(trainedModel);
+
+            foreach (var heartData in HeartSampleData.heartDataList)
+            {
+                var prediction = predictionEngine.Predict(heartData);
+
+                Console.WriteLine($"=============== Single Prediction  ===============");
+                Console.WriteLine($"Age: {heartData.Age} ");
+                Console.WriteLine($"Sex: {heartData.Sex} ");
+                Console.WriteLine($"Cp: {heartData.Cp} ");
+                Console.WriteLine($"TrestBps: {heartData.TrestBps} ");
+                Console.WriteLine($"Chol: {heartData.Chol} ");
+                Console.WriteLine($"Fbs: {heartData.Fbs} ");
+                Console.WriteLine($"RestEcg: {heartData.RestEcg} ");
+                Console.WriteLine($"Thalac: {heartData.Thalac} ");
+                Console.WriteLine($"Exang: {heartData.Exang} ");
+                Console.WriteLine($"OldPeak: {heartData.OldPeak} ");
+                Console.WriteLine($"Slope: {heartData.Slope} ");
+                Console.WriteLine($"Ca: {heartData.Ca} ");
+                Console.WriteLine($"Thal: {heartData.Thal} ");
+                Console.WriteLine($"Prediction Value: {prediction.Prediction} ");
+                Console.WriteLine($"Prediction: {(prediction.Prediction ? "A disease could be present" : "Not present disease")} ");
+                Console.WriteLine($"Probability: {prediction.Probability} ");
+                Console.WriteLine($"==================================================");
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+
+        }
+
+    }
+}
